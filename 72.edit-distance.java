f48fd672efe133cc9216315632f73f169c0d2315
svsf91 <svsf91@126.com>
2018-10-03 16:29:32 -0400

    update 72.edit-distance.java

---
+++ 72.edit-distance.java
@@ -0,0 +1,78 @@
+/*
+ * [72] Edit Distance
+ *
+ * https://leetcode.com/problems/edit-distance/description/
+ *
+ * algorithms
+ * Hard (34.33%)
+ * Total Accepted:    135.1K
+ * Total Submissions: 393.5K
+ * Testcase Example:  '"horse"\n"ros"'
+ *
+ * Given two words word1 and word2, find the minimum number of operations
+ * required to convert word1 to word2.
+ * 
+ * You have the following 3 operations permitted on a word:
+ * 
+ * 
+ * Insert a character
+ * Delete a character
+ * Replace a character
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: word1 = "horse", word2 = "ros"
+ * Output: 3
+ * Explanation: 
+ * horse -> rorse (replace 'h' with 'r')
+ * rorse -> rose (remove 'r')
+ * rose -> ros (remove 'e')
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: word1 = "intention", word2 = "execution"
+ * Output: 5
+ * Explanation: 
+ * intention -> inention (remove 't')
+ * inention -> enention (replace 'i' with 'e')
+ * enention -> exention (replace 'n' with 'x')
+ * exention -> exection (replace 'n' with 'c')
+ * exection -> execution (insert 'u')
+ * 
+ * 
+ */
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+
+        //TODO: set initial value
+        for(int[] row: dp) {
+            Arrays.fill(row, Integer.MAX_VALUE);
+        }
+        dp[0][0] = 0;
+        for(int i = 0; i < word1.length(); i++) {
+            dp[i + 1][0] = i + 1;
+        }
+        for(int j = 0; j < word2.length(); j++) {
+            dp[0][j + 1] = j + 1;
+        }
+
+        for(int i = 0; i < word1.length(); i++) {
+            for(int j = 0; j < word2.length(); j++) {
+                char c1 = word1.charAt(i);
+                char c2 = word2.charAt(j);
+                if(c1 == c2) {
+                    dp[i + 1][j + 1] = dp[i][j];
+                } else {
+                    dp[i + 1][j + 1] = Math.min(dp[i][j + 1], Math.min(dp[i + 1][j], dp[i][j])) + 1;
+                }
+            }
+        }
+
+        return dp[word1.length()][word2.length()];
+    }
+}
